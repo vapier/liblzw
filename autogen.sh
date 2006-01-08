73e3330985b13d03d811862d09940fc56d30a8e6
Mike Frysinger <vapier@gmail.com>
2006-01-08 05:57:47 +0000

    use new common m4 files
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-aclocal || exit 1
+
+if test -d CVS ; then
+	if test -d ../m4 ; then
+		rm -rf m4 || exit 1
+		mkdir -p m4 || exit 1
+		cp ../m4/*.m4 m4/ || exit 1
+	fi
+fi
+
+aclocal -I m4 || exit 1
 autoheader || exit 1
 libtoolize --automake -c -f || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
 
-if [ -x ./test.sh ] ; then
+if test -x ./test.sh ; then
 	exec ./test.sh "$@"
 fi
