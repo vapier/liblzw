50bba6da1ee240cbb256c2353a816061b005663f
Mike Frysinger <vapier@gmail.com>
2006-09-28 10:05:43 +0000

    simpler autogen scripts
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,14 @@
-#!/bin/sh
+#!/bin/sh -xe
 
 if test -d .svn ; then
 	if test -d ../m4 ; then
-		rm -rf m4 || exit 1
-		mkdir -p m4 || exit 1
-		cp ../m4/*.m4 m4/ || exit 1
+		rm -rf m4
+		mkdir -p m4
+		cp ../m4/*.m4 m4/
 	fi
 fi
 
-aclocal -I m4 || exit 1
-autoheader || exit 1
-libtoolize --automake -c -f || exit 1
-autoconf || exit 1
-automake -a -c || exit 1
+autoreconf
 
 if test -x ./test.sh ; then
 	exec ./test.sh "$@"
