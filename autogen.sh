8de68f85423447d3f4274412f792b957a3ae713b
Mike Frysinger <vapier@gmail.com>
2010-02-08 20:07:36 -0500

    overhaul build system after svn->git breakup
    Let liblzw stand on its own without libstdf, generally clean up the
    autotool files, and add gitignore files.
    
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,9 @@
 #!/bin/sh -xe
 
-if test -d .svn ; then
-	if test -d ../m4 ; then
-		rm -rf m4
-		mkdir -p m4
-		cp ../m4/*.m4 m4/
-		cp ../m4/*.at tests/
-	fi
-fi
-
 autoreconf -i -f
 
 # broken autotools wrongly clobbers our INSTALL
-svn revert INSTALL >& /dev/null || true
+git checkout -f INSTALL || true
 
 if test -x ./test.sh ; then
 	exec ./test.sh "$@"
