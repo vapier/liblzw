b564664e6738bce1ff754bfed8e2da1d4e73b329
Mike Frysinger <vapier@gmail.com>
2021-02-15 08:38:41 -0500

    refresh build tools
    Force automake-1.15 in the process.  Anyone generating autotools
    should have a release from 6+ years ago.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -xe
 
+# Clobber files to avoid sticking to old autotools versions.
+rm -f aclocal.m4 Makefile Makefile.in configure
+
 mkdir -p m4
 autoreconf -i -f
 
-# broken autotools wrongly clobbers our INSTALL
+# Broken autotools wrongly clobbers our INSTALL.
 git checkout -f INSTALL || true
